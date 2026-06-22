0f63ccaf200ed24aff9c1ba01407e51ac2714ea8
hatory812 <tsukubahatoryiaki@gmail.com>
2026-06-22 14:18:49 +0900

    fix: データセットのパス変更・入力する特徴量のモード変更

--- run/degree/binary.sh
+++ run/degree/binary.sh
@@ -1,5 +1,5 @@
 small_dir="./small_graphs"
-all_dir="./all_graphs"
+all_dir="/hss01/A.hattori/rww_all_graphs/degree/mid"
 data_type="all"
 output_dim=2
 mv=0
@@ -9,7 +9,7 @@ rww="degree"
 m1="GCN"
 gcn_lr=0.0001
 gcn_hl=1024
-node_attr_gcn=0
+node_attr_gcn=1
 python3 ../train_twitter_MPNN.py --model $m1 --data_type $data_type --multivariate $mv --hidden_dim $gcn_hl --lr $gcn_lr --output_dim $output_dim --small_graphs_path $small_dir --all_graphs_path $all_dir --rww_attr $rww --node_attr $node_attr_gcn
 
 #GAT
@@ -23,12 +23,12 @@ python3 ../train_twitter_MPNN.py --model $m2 --data_type $data_type --multivaria
 m3="GIN"
 gin_lr=0.0001
 gin_hl=512
-node_attr_gin=0
+node_attr_gin=1
 python3 ../train_twitter_MPNN.py --model $m3 --data_type $data_type --multivariate $mv --hidden_dim $gin_hl --lr $gin_lr --output_dim $output_dim --small_graphs_path $small_dir --all_graphs_path $all_dir --rww_attr $rww --node_attr $node_attr_gin
 
 #SAGE
 m4="SAGE"
 gine_lr=0.0001
 gine_hl=1024
-node_attr_sage=0
+node_attr_sage=1
 python3 ../train_twitter_MPNN.py --model $m4 --data_type $data_type --multivariate $mv --hidden_dim $gine_hl --lr $gine_lr --output_dim $output_dim --small_graphs_path $small_dir --all_graphs_path $all_dir --rww_attr $rww --node_attr $node_attr_sage
